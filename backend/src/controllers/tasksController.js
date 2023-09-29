708d94bd11248ee48bb911db837b8c4354b3abd9
João Vítor <jvpeniza@gmail.com>
2023-09-29 16:12:03 -0300

    Criando inserção de dados

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -6,6 +6,13 @@ const getAll = async(req, res) => {
     return res.status(200).json(tasks);
 };
 
+const createTask = async(req, res) => {
+    const createdTask = await tasksModel.createTask(req.body);
+
+    return res.status(201).json(createdTask);
+};
+
 module.exports = {
-    getAll
+    getAll,
+    createTask
 };
--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -5,6 +5,19 @@ const getAll = async() => {
     return tasks;
 };
 
+const createTask = async(task) => {
+
+    const { title } = task;
+
+    const dateUTC = new Date().toUTCString();
+
+    const query = `INSERT INTO tasks(title, status, created_at) VALUES ('${title}', 'pendente', '${dateUTC}')`;
+ 
+    const [createdTask] = await connection.execute(query);
+    return { insertId: createdTask.insertId };
+}
+
 module.exports = {
-    getAll
+    getAll,
+    createTask,
 };
